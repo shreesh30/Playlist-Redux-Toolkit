ff47ad3fcdf1fd4a3e55215ca797ae07322d0c74
Shreesh Arya <shreesharya20@gmail.com>
2023-07-01 15:02:00 +0530

    Initial commit

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const songSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {
+      state.pop(action.payload);
+    }
+  }
+});
+
+const store = configureStore({
+  reducer: {
+    songs: songSlice.reducer
+  }
+});
+
+export { store };
